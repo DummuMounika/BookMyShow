4b1e39021e37b95f8469effa8856d71c2913b327
DummuMounika <mounikadummu.edu2024@gmail.com>
2025-03-27 01:26:13 -0500

    Added new abstract methods for v2 version

--- search-service/src/main/java/com/project/microservices/searchservice/movie/service/MovieService.java
+++ search-service/src/main/java/com/project/microservices/searchservice/movie/service/MovieService.java
@@ -3,16 +3,19 @@ package com.project.microservices.searchservice.movie.service;
 import java.util.List;
 
 import com.project.microservices.searchservice.model.SearchQueryResponse;
-
-import jakarta.validation.constraints.NotBlank;
+import com.project.microservices.searchservice.model.SearchQueryResponse1;
 
 public interface MovieService {
 	
 	public List<SearchQueryResponse> findTheatersByMovieNameAndTheaterCity(String movieName, String theaterCity);
+	
+	public List<SearchQueryResponse1> findTheatersByMovieIdAndTheaterId(Integer movieId, Integer theaterId);
 
 	public List<String> findByMovieName(String movieName);
 
 	public List<String> findMoviesByCity(String cityName);
+
+	public List<String> findMoviesByCityId(Integer cityId);
 
 	
 }
--- search-service/src/main/java/com/project/microservices/searchservice/service/SearchService.java
+++ search-service/src/main/java/com/project/microservices/searchservice/service/SearchService.java
@@ -4,11 +4,18 @@ import java.util.List;
 
 import com.project.microservices.searchservice.model.SearchResponse;
 
+import jakarta.validation.constraints.NotBlank;
+
 public interface SearchService {
 
 	public SearchResponse findTheatersByMovieNameAndTheaterCity(String movieName, String theaterCity);
 
+	public SearchResponse findTheatersByMovieIdAndTheaterCityId(Integer movieId, Integer theaterCityId);
+	
 	public List<String> getAllCities();
+	
+	public List<String> getAllCities1();
 
+	
 
 }
