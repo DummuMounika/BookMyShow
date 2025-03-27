bb137170b78b1ad37cae83e8c839c6075aecc0b3
DummuMounika <mounikadummu.edu2024@gmail.com>
2025-03-27 01:28:46 -0500

    Renamed the file name

--- user-service/src/main/java/com/project/microservices/userservice/exception/NotFoundException.java
+++ user-service/src/main/java/com/project/microservices/userservice/exception/NotFoundException.java
@@ -3,15 +3,16 @@ package com.project.microservices.userservice.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.NOT_FOUND)
-public class UserNotFoundException  extends RuntimeException {
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class NotFoundException  extends RuntimeException {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
-	public UserNotFoundException(boolean b, String string) {
+	public NotFoundException(boolean status,String message) {
+		super(message);
 	}
 
 }
