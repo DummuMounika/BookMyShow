b09d2714e39730d674c06ff3132993e26e267ecd
DummuMounika <mounikadummu.edu2024@gmail.com>
2025-03-27 21:30:15 -0500

    Changes are made on return type and query

--- search-service/src/main/java/com/project/microservices/searchservice/show/repository/ShowRepository.java
+++ search-service/src/main/java/com/project/microservices/searchservice/show/repository/ShowRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.project.microservices.searchservice.movie.model.MovieResponse;
 import com.project.microservices.searchservice.show.entity.ShowEntity;
 import com.project.microservices.searchservice.show.model.ShowDetailsQueryResponse;
 
@@ -30,11 +31,13 @@ public interface ShowRepository extends JpaRepository<ShowEntity, Integer> {
 		     + "WHERE t.theaterCity = :city")
 		List<String> searchByCitiesToGetMovies(@Param("city") String city);
 	
-	@Query(value = "SELECT DISTINCT m.movieName FROM ShowEntity s "
-		     + "JOIN TheaterEntity t ON s.showTheaterId = t.theaterId "
-		     + "JOIN MovieEntity m ON s.showMovieId = m.movieId "
-		     + "WHERE t.theaterCityId = :cityId")
-		List<String> searchByCitiesToGetMovies1(@Param("cityId") Integer cityId);
+	@Query("SELECT new com.project.microservices.searchservice.movie.model.MovieResponse(" +
+            "m.movieId, m.movieName) " +
+        "FROM ShowEntity s " +
+        "JOIN TheaterEntity t ON s.showTheaterId = t.theaterId " +
+        "JOIN MovieEntity m ON s.showMovieId = m.movieId " +
+        "WHERE t.theaterCityId = :cityId")
+		List<MovieResponse> searchByCitiesToGetMovies1(@Param("cityId") Integer cityId);
 
 
 
